81f5cfc71140509a4e2e2fcc62dd0d531faf5c8c
Miguel Angel Martin Hrdez <miguel.martin@analyticalways.com>
2017-10-04 10:15:36 +0200

    FizzBuzzShould return_fizz_when_param_is_divisible_by_3

--- FizzBuzz/FizzBuzzShould.cs
+++ FizzBuzz/FizzBuzzShould.cs
@@ -12,12 +12,24 @@ namespace FizzBuzz
             string result = fizzBuzz.Translate(1);
             result.Should().Be("1");
         }
+
+        [Fact]
+        public void return_fizz_when_param_is_divisible_by_3()
+        {
+            var fizzBuzz = new FizzBuzz();
+            string result = fizzBuzz.Translate(3);
+            result.Should().Be("fizz");
+        }
     }
 
     public class FizzBuzz
     {
         public string Translate(int number)
         {
+            if (number % 3 == 0)
+            {
+                return "fizz";
+            }
             return number.ToString();
         }
     }
