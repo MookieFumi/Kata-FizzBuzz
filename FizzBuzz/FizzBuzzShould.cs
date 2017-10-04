cb28ea82f55c8f9687b827d044b5402e77f1479c
Miguel Angel Martin Hrdez <miguel.martin@analyticalways.com>
2017-10-04 10:12:39 +0200

    FizzBuzzShould return_1_when_param_is_1

--- FizzBuzz/FizzBuzzShould.cs
+++ FizzBuzz/FizzBuzzShould.cs
@@ -1,12 +1,24 @@
-﻿using Xunit;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace FizzBuzz
 {
     public class FizzBuzzShould
     {
         [Fact]
-        public void TestMethod1()
+        public void return_1_when_param_is_1()
         {
+            var fizzBuzz = new FizzBuzz();
+            string result = fizzBuzz.Translate(1);
+            result.Should().Be("1");
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public string Translate(int number)
+        {
+            return number.ToString();
         }
     }
 }
